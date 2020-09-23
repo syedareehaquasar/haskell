0be2472480845b161a4d9094bbc9cd0c97a1b5c1
syedareehaquasar <reeha.saba@gmail.com>
2020-09-23 09:35:44 +0530

    isBetterPrime added

--- IsPrimeBetter.hs
+++ IsPrimeBetter.hs
@@ -1,1 +1,15 @@
-divides :: 
+divides :: Int -> Int -> Bool
+divides d n = rem n d == 0
+
+firstFactorAfter :: Int -> Int -> Int
+firstFactorAfter k n | divides k n = k
+firstFactorAfter k n | k * k > n   = n
+firstFactorAfter k n | otherwise   = firstFactorAfter (k + 1) n
+
+firstFactor :: Int -> Int
+firstFactor n = firstFactorAfter 2 n
+
+isPrime :: Int -> Bool
+isPrime n | n < 0     = error "Not a prime it's negative or zero"
+isPrime n | n == 1    = False
+isPrime n | otherwise = firstFactor n == n

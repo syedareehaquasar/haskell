e8e742942d1600aca83be067691cdb46e9f4b806
syedareehaquasar <reeha.saba@gmail.com>
2020-10-06 01:01:43 +0530

    tried doing codelab1 yet not complete

--- codelab1.1.hs
+++ codelab1.1.hs
@@ -1,10 +1,8 @@
 notEndsInZero :: Int -> Bool
 notEndsInZero n = rem n 10 /= 0
 
-select :: [Int] -> Int -> Int -> [Int]
-select array index1 index2 = drop index1 $ take index2 array
-
--- select' :: [Int] -> Int -> [[Int]]
+-- select :: [Int] -> Int -> Int -> [Int]
+-- select array index1 index2 = drop index1 $ take index2 array
 
 trailingInZeros' :: Int -> Int
 trailingInZeros' n | notEndInZero n = 0
@@ -13,10 +11,17 @@ trailingInZeros' n | otherwise      = 1 + (trailingInZeros' $ quot n 10)
 trailingInZeros :: [Int] -> Int
 trailingInZeros nums = trailingzeros' $ product nums
 
-codelab1 :: [Int] -> Int -> Int
-codelab1 array k = maximum $ _
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations n xs = [ xs !! i : x | i <- [0..(length xs)-1] , x <- combinations (n-1) (drop (i+1) xs) ]
+
+codelab1 :: [Int] -> Int -> Int 
+codelab1 array k = maximum $ trailingInZeros $ [pick_from !! i | i <- [0..length (pick_from) - 1], pick_from <- combinations k array]
+
+-- atoi :: String -> Int
+-- atoi s = read s :: Int
 
 main = do
-    array <- getLine
-    n <- getLine
-    print $ codelab1(read array :: [Int])
+    -- array <- getLine
+    -- n <- getLine
+    print $ codelab1 [1, 2, 3, 4] 2

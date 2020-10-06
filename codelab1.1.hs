d879f2eb9f47e6e03c73808456d505003e51c134
syedareehaquasar <reeha.saba@gmail.com>
2020-10-06 07:03:27 +0530

    modifying products part

--- codelab1.1.hs
+++ codelab1.1.hs
@@ -4,19 +4,19 @@ notEndsInZero n = rem n 10 /= 0
 -- select :: [Int] -> Int -> Int -> [Int]
 -- select array index1 index2 = drop index1 $ take index2 array
 
-trailingInZeros' :: Int -> Int
-trailingInZeros' n | notEndInZero n = 0
-trailingInZeros' n | otherwise      = 1 + (trailingInZeros' $ quot n 10)
+countZeros :: Int -> Int
+countZeros n | notEndInZero n = 0
+countZeros n | otherwise      = 1 + (countZeros $ quot n 10)
 
-trailingInZeros :: [Int] -> Int
-trailingInZeros nums = trailingzeros' $ product nums
+-- trailingInZeros :: [Int] -> Int
+-- trailingInZeros nums = countZeros $ product nums
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _ = [[]]
-combinations n xs = [ xs !! i : x | i <- [0..(length xs)-1] , x <- combinations (n-1) (drop (i+1) xs) ]
+combinations n xs = [ product (xs !! i : x) | i <- [0..(length xs)-1] , x <- combinations (n-1) (drop (i+1) xs) ]
 
-codelab1 :: [Int] -> Int -> Int 
-codelab1 array k = maximum $ trailingInZeros $ [pick_from !! i | i <- [0..length (pick_from) - 1], where pick_from = combinations k array]
+-- codelab1 :: [Int] -> Int -> Int 
+-- codelab1 array k = maximum $ trailingInZeros $ [pick_from !! i | i <- [0..length (pick_from) - 1], where pick_from = combinations k array]
 
 -- atoi :: String -> Int
 -- atoi s = read s :: Int
@@ -24,4 +24,5 @@ codelab1 array k = maximum $ trailingInZeros $ [pick_from !! i | i <- [0..length
 main = do
     -- array <- getLine
     -- n <- getLine
-    print $ codelab1 [1, 2, 3, 4] 2
+    print $ combinations 2 [1, 2, 3, 4]
+    -- print $ codelab1 [1, 2, 3, 4] 2

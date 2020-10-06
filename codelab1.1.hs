7bb473773438ab1eff085b2aef2a32cdf62606a6
syedareehaquasar <reeha.saba@gmail.com>
2020-10-06 07:21:31 +0530

    codelab1 solved

--- codelab1.1.hs
+++ codelab1.1.hs
@@ -2,18 +2,17 @@ notEndsInZero :: Int -> Bool
 notEndsInZero n = rem n 10 /= 0
 
 countZeros :: Int -> Int
-countZeros n | notEndInZero n = 0
+countZeros n | notEndsInZero n = 0
 countZeros n | otherwise      = 1 + (countZeros $ quot n 10)
-
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _ = [[]]
 combinations n xs = [ xs !! i : x | i <- [0..(length xs)-1] , x <- combinations (n-1) (drop (i+1) xs) ]
 
--- codelab1 :: [Int] -> Int -> Int 
--- codelab1 array k = maximum $ trailingInZeros $ [pick_from !! i | i <- [0..length (pick_from) - 1], where pick_from = combinations k array]
+productsOfSubsets :: [Int] -> Int -> [Int]
+productsOfSubsets array k =  [product s | s <- combinations k array]
 
+codelab1 :: [Int] -> Int -> Int 
+codelab1 array k = maximum $ map countZeros $ productsOfSubsets array k
 
-main = do
-    print $ combinations 2 [1, 2, 3, 4]
-    -- print $ codelab1 [1, 2, 3, 4] 2
+main = print $ codelab1 [50, 4, 20] 2

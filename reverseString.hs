886c3ea34a37c53075a668f47765d043b2185f56
syedareehaquasar <syedareehaquasar@gmail.com>
2020-11-03 08:23:35 +0530

    optimised and better syntax used point free

--- reverseString.hs
+++ reverseString.hs
@@ -1,4 +1,5 @@
 revStr :: String -> String
-revStr s = unwords(reverse(words(s)))
+-- revStr s = unwords(reverse(words(s)))
+revStr = unwords . reverse . words
 
 main = print (revStr("you can't lose can you"))

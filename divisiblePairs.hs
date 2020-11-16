6c897e75507cd46af1d6a10dc61884711fcaaa65
syedareehaquasar <syedareehaquasar@gmail.com>
2020-11-16 22:23:16 +0530

    added main and made more readable

--- divisiblePairs.hs
+++ divisiblePairs.hs
@@ -1,2 +1,9 @@
 divisiblePairs :: Int -> Int -> [Int] -> Int
-divisiblePairs n k arr = length [(i, j) | i <- [0..length arr - 2], j <- [0..length arr - 1], (arr !! i + arr !! j) `mod` k == 0, i < j]
+divisiblePairs n k arr = length [(i, j) | i <- [0..length arr - 2],
+                                          j <- [0..length arr - 1],
+                                          (arr !! i + arr !! j) `mod` k == 0,
+                                          i < j]
+
+main = do
+print $ divisiblePairs 6 3 [1, 3, 2, 6, 1, 2]
+

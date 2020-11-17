3aa2c0448bcfb26225f6b3bdff6b73a4b80a8cbd
syedareehaquasar <syedareehaquasar@gmail.com>
2020-11-17 10:06:43 +0530

    more haskell iconic code

--- divisiblePairs.hs
+++ divisiblePairs.hs
@@ -1,9 +1,26 @@
-divisiblePairs :: Int -> Int -> [Int] -> Int
-divisiblePairs n k arr = length [(i, j) | i <- [0..length arr - 2],
+divisiblePairs ::  Int -> [Int] -> Int
+divisiblePairs k arr = length [(i, j) | i <- [0..length arr - 2],
                                           j <- [0..length arr - 1],
                                           (arr !! i + arr !! j) `mod` k == 0,
                                           i < j]
 
+isMultiple :: Int -> Int -> Bool
+isMultiple k x = rem x k == 0
+
+divisibleSumPairs :: [Int] -> Int -> Int
+divisibleSumPairs arr k = length $ filter (isMultiple k) [(head arr) + t | t <- tail arr]
+
+countDivSumPairs :: [Int] -> Int -> Int
+countDivSumPairs x:xs k = length $ filter (isMultiple k) [x + t | t <- xs]
+
+countAllDivSumPairs :: [Int] -> Int -> Int
+countAllDivSumPairs [] k = 0
+countAllDivSumPairs [x] k = 0
+countAllDivSumPairs arr k = divisibleSumPairs arr k + (countAllDivSumPairs (tail arr) k)
+
+countAllDivSumPairs 
+
 main = do
-print $ divisiblePairs 6 3 [1, 3, 2, 6, 1, 2]
+print $ divisiblePairs 3 [1, 3, 2, 6, 1, 2]
+print $ countAllDivSumPairs [1, 3, 2, 6, 1, 2] 3
 

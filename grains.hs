01e2bd02a5e0ea21d9b290286b0a3f9b22f373c4
reeha <56428237+syedareehaquasar@users.noreply.github.com>
2021-03-31 09:51:26 +0530

    Update grains.hs

--- grains.hs
+++ grains.hs
@@ -1,3 +1,5 @@
+import Data.Maybe
+
 square :: Integer -> Maybe Integer
 square n | n < 1 || n > 64 =  Nothing
          | otherwise = Just $ 2 ^ (n - 1) 
@@ -5,9 +7,12 @@ square n | n < 1 || n > 64 =  Nothing
 total :: Integer
 total = maybe 0 (\n -> 2*n - 1) $ square 64
 
+total1 :: Num a => a
+total1 = sum(map square x) where x = [1,2..64]
+
 main = do
   print $ square(12)
   print $ total
-  print $ "Enter the grains you want to know of a square"
+  putStrLn ("Enter the grains you want to know of a square")
   n <- getLine
   print(n)
